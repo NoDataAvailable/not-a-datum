87bdd2fbf7b8562871a23a87568289a04a4dc4a1
Roberto Bortolussi <robot.bobert@gmail.com>
2012-09-22 23:15:37 -0400

    Test pages added, previous problems fixed.

--- not_a_datum/views.py
+++ not_a_datum/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("<h1>No Data To Be Found!</h1> None at all")
+
 def hello(request):
     return HttpResponse("Hello world")
